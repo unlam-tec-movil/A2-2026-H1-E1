833a21cf04ae46c3843a0f43348816ff31a4dcee
Alirag <alan.irigoin@gmail.com>
2026-06-24 21:38:40 -0300

    Fix ktlint issues

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/PostCreateForm.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/PostCreateForm.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -17,7 +16,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -30,7 +28,7 @@ fun PostCreateForm(
 
     Column(
         modifier = Modifier.padding(16.dp)
-    ){
+    ) {
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -46,7 +44,7 @@ fun PostCreateForm(
             // TextField
             OutlinedTextField(
                 value = postText,
-                onValueChange = { onPostTextChange(it)},
+                onValueChange = { onPostTextChange(it) },
                 placeholder = { Text("¿Qué estás pensando?") },
                 modifier = Modifier.weight(1f)
             )
