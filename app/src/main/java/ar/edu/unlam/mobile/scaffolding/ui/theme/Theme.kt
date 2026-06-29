528acc1479108f8d59f9472ebfe8b5f9ef9376ba
Maximo <assadmaximo959@gmail.com>
2026-06-29 19:34:39 -0300

    theme

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/theme/Theme.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/theme/Theme.kt
@@ -17,25 +17,16 @@ import androidx.core.view.WindowCompat
 
 private val DarkColorScheme =
     darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
+        primary = Blue80,
+        secondary = BlueGrey80,
+        tertiary = LightBlue80,
     )
 
 private val LightColorScheme =
     lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-        /* Other default colors to override
-        background = Color(0xFFFFFBFE),
-        surface = Color(0xFFFFFBFE),
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.White,
-        onBackground = Color(0xFF1C1B1F),
-        onSurface = Color(0xFF1C1B1F),
-         */
+        primary = Blue40,
+        secondary = BlueGrey40,
+        tertiary = LightBlue40,
     )
 
 @Composable
