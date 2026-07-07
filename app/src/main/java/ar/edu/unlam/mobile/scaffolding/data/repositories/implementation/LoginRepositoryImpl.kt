27eda31df84dbddeb19d240240370ee1799a047e
palocipriano <palocipriano@gmail.com>
2026-07-07 16:08:05 -0300

    Update LoginRepositoryImpl.kt

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repositories/implementation/LoginRepositoryImpl.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repositories/implementation/LoginRepositoryImpl.kt
@@ -1,3 +1,11 @@
+package ar.edu.unlam.mobile.scaffolding.data.repositories.implementation
+
+import ar.edu.unlam.mobile.scaffolding.data.datasources.network.models.interfaces.TuiterApiService
+import ar.edu.unlam.mobile.scaffolding.data.datasources.network.models.login.LoginRequest
+import ar.edu.unlam.mobile.scaffolding.data.datasources.network.models.login.LoginResponse
+import ar.edu.unlam.mobile.scaffolding.data.repositories.interfaces.LoginRepository
+import javax.inject.Inject
+
 class LoginRepositoryImpl
     @Inject
     constructor(
